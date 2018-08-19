9b137bf46d25b30c1e05bdf823252f399eddc036
Bishoy Maher <beshoi.maher7@gmail.com>
2018-08-19 21:26:53 +0200

    refactor:  Reduce connect code into function connect()
    Minor Improvements:
    - Add Shebang Line
    - Remove unnecessary library imports

--- report.py
+++ report.py
@@ -1,14 +1,16 @@
+#! /usr/bin/env python3
 import psycopg2
-from datetime import datetime
-from datetime import time
-from datetime import date
 
-
-DBNAME = "news"
+def connect(DBNAME = "news"):
+    try:
+        db = psycopg2.connect(dbname=DBNAME)
+        c = db.cursor()
+        return db, c
+    except psycopg2.DatabaseError, e:
+        print("Sorry! Unable to Connect to Database!")
 
 #TASK 1
-db = psycopg2.connect(dbname=DBNAME)
-c = db.cursor()
+db, c = connect()
 query = "SELECT title, count(*) AS views FROM (SELECT REPLACE(path, '/article/', '') AS newpath, * FROM log) AS newlog JOIN articles ON articles.slug = newlog.newpath  GROUP BY title ORDER BY views DESC LIMIT 3;"
 c.execute(query)
 topthree = c.fetchall()
@@ -21,8 +23,7 @@ db.close()
 print
 
 #TASK 2
-db = psycopg2.connect(dbname=DBNAME)
-c = db.cursor()
+db, c = connect()
 query = "SELECT name, count(*) AS views FROM (SELECT REPLACE(path, '/article/', '') AS newpath, * FROM log) AS newlog JOIN articles ON articles.slug = newlog.newpath JOIN authors ON authors.ID = articles.author  GROUP BY name ORDER BY views DESC;"
 c.execute(query)
 popular_authors = c.fetchall()
@@ -37,8 +38,7 @@ db.close()
 print
 
 #TASK 3
-db = psycopg2.connect(dbname=DBNAME)
-c = db.cursor()
+db, c = connect()
 query = "SELECT date, errper FROM  (SELECT  time::timestamp::date AS date, ROUND((err * 100.0) / req, 3) AS errper FROM (SELECT a.time::timestamp::date, count(*) AS req, count(*) FILTER (WHERE a.status != '200 OK') AS err FROM log AS a, log AS b WHERE a.id = b.id GROUP BY a.time::timestamp::date ORDER BY a.time::timestamp::date) AS reduced) AS postproc WHERE errper > 1.0;"
 c.execute(query)
 badday = c.fetchall()
