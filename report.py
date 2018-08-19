532ab6c5de3dd38f3e62cc44ad9d9a9d423c547e
Bishoy Maher <beshoi.maher7@gmail.com>
2018-08-19 22:02:34 +0200

    refactor: Reduce line length of SQL queries
    Used triple quotes for code block instead of single quotes.

--- report.py
+++ report.py
@@ -12,7 +12,11 @@ def connect(DBNAME = "news"):
 def task1():
     db, c = connect()
 
-    query = "SELECT title, count(*) AS views FROM (SELECT REPLACE(path, '/article/', '') AS newpath, * FROM log) AS newlog JOIN articles ON articles.slug = newlog.newpath  GROUP BY title ORDER BY views DESC LIMIT 3;"
+    query = """SELECT title, count(*) AS views
+    FROM (SELECT REPLACE(path, '/article/', '') AS newpath, * FROM log) AS newlog
+    JOIN articles ON articles.slug = newlog.newpath
+    GROUP BY title
+    ORDER BY views DESC LIMIT 3;"""
 
     c.execute(query)
     topthree = c.fetchall()
@@ -25,7 +29,12 @@ def task1():
 def task2():
     db, c = connect()
 
-    query = "SELECT name, count(*) AS views FROM (SELECT REPLACE(path, '/article/', '') AS newpath, * FROM log) AS newlog JOIN articles ON articles.slug = newlog.newpath JOIN authors ON authors.ID = articles.author  GROUP BY name ORDER BY views DESC;"
+    query = """SELECT name, count(*) AS views
+    FROM (SELECT REPLACE(path, '/article/', '') AS newpath, * FROM log) AS newlog
+    JOIN articles ON articles.slug = newlog.newpath
+    JOIN authors ON authors.ID = articles.author
+    GROUP BY name
+    ORDER BY views DESC;"""
 
     c.execute(query)
     popular_authors = c.fetchall()
@@ -40,7 +49,16 @@ def task2():
 def task3():
     db, c = connect()
 
-    query = "SELECT date, errper FROM  (SELECT  time::timestamp::date AS date, ROUND((err * 100.0) / req, 3) AS errper FROM (SELECT a.time::timestamp::date, count(*) AS req, count(*) FILTER (WHERE a.status != '200 OK') AS err FROM log AS a, log AS b WHERE a.id = b.id GROUP BY a.time::timestamp::date ORDER BY a.time::timestamp::date) AS reduced) AS postproc WHERE errper > 1.0;"
+    query = """SELECT date, errper
+    FROM  (SELECT  time::timestamp::date AS date, ROUND((err * 100.0) / req, 3) AS errper
+        FROM (SELECT a.time::timestamp::date, count(*) AS req, count(*) FILTER
+            (WHERE a.status != '200 OK') AS err
+            FROM log AS a, log AS b
+            WHERE a.id = b.id
+            GROUP BY a.time::timestamp::date
+            ORDER BY a.time::timestamp::date) AS reduced)
+        AS postproc
+    WHERE errper > 1.0;"""
 
     c.execute(query)
     badday = c.fetchall()
